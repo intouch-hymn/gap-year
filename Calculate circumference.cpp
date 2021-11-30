f1031023338f78f29c4f3e840ed772772f16e8f8
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2021-11-30 21:53:30 +0700

    Add files via upload

---
+++ Calculate circumference.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float r,Pi = 3.14;
+    printf("--- WELCOME TO CALCULATE CIRCUMFERENCE OF CIRCLE PROGRAM ---\n");
+    printf("Please insert Radius : ");
+    scanf("%f",&r);
+    printf("\nThe circumference of a circle is %.2f", 2 * Pi * r);
+    return 0;
+    //Intouch Krajangprateep No.4 M.6/1
+}
