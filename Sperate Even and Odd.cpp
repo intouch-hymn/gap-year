669b175eb086c95f271a433a3537eccba1099b1f
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2021-12-04 09:17:36 +0700

    Add files via upload

---
+++ Sperate Even and Odd.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num,ans;
+	printf("---Wecome to Sperate Even and Odd Number Program ---\n");
+ 	printf("Please insert your number : ");
+ 	scanf("%d",&num);
+ 	ans = num%2;
+ 	printf("%d",ans);
+//Intouch Krajangprateep No.4 M.6/1
+}
+
+
