031c55244ddfe2ab2d15aba4053d8e1b39b2d7ec
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2022-01-20 21:43:49 +0700

    Add files via upload

---
+++ Sperate Odd and Even number program.py
@@ -0,0 +1,7 @@
+#Sperate Odd and Even number program
+print("---Wecome to Sperate Even and Odd Number Program ---\n")
+number = int(input("Please insert your number : "))
+if number % 2 == 0 :
+    print('Even number')
+else :
+    print('Odd number')
