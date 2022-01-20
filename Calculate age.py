846fe4ccc46282fc1c9955bc593507bf4fafc0b1
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2022-01-20 21:11:21 +0700

    Add files via upload

---
+++ Calculate age.py
@@ -0,0 +1,19 @@
+
+
+age = int(input('Please insert your age '))
+
+if age >= 15 and age <= 20 :        
+    print('Teenager')
+elif age >= 20 and age <=29:
+    print('Young age')
+elif age >= 30 and age <= 39:
+    print('Adult')
+elif age >= 60 :
+    print('Old age')
+else :
+    print('Childhood')
+    
+print('Program End')
+
+
+#if condition : 
