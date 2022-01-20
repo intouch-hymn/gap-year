f56ac85bc4cbb1fe09ff070addd9923b060d6022
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2022-01-20 21:35:57 +0700

    Add files via upload

---
+++ Compare number.py
@@ -0,0 +1,10 @@
+#Compare number program
+a=int(input('ป้อนตัวเลขที่ 1 :'))
+b=int(input('ป้อนตัวเลขที่ 2 :'))
+
+if a>b :
+    print(str(a)+'more than'+ str(b))
+elif a ==b :
+    print(str(a)+' equal to '+str(b))
+else :
+    print(str(a)+'less than'+ str(b))
