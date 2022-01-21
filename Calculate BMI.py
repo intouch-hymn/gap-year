d3cd115bee56c43bd8181e667f9c1ac38c5873fd
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2022-01-21 07:25:32 +0700

    Add files via upload

--- Calculate BMI.py
+++ Calculate BMI.py
@@ -5,9 +5,10 @@
 weight = int(input('Please insert your weight (kg) :')) 
 height = int(input('Please insert your height (cm):'))
 
+
 #process
 #cm->m
-height /=100
+height /= 100
 bmi = weight/height**2 #calculate BMI
 
 #output
