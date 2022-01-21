d3497726cccc64f282a7e1dbc01dc3e678e1157c
intouch-hymn <69072176+intouch-hymn@users.noreply.github.com>
2022-01-21 07:35:06 +0700

    Add files via upload

---
+++ A.D. to B.E. .py
@@ -0,0 +1,11 @@
+#A.D. to B.E.
+print("---Wecome to A.D._to_B.E. Program ---\n")
+#input
+number=int(input('Please insert your A.D. :'))
+
+#process
+
+number += 543
+
+#output
+print('This is ',number,'B.E.')
